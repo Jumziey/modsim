1d19c2ef703f9fa9e2e9ac8ec95bc193a5df0145
Jesper Vesterberg <jumziey@gmail.com>
2014-11-03 19:37:24 +0100

    Major change

--- upg1/plots/ex41.m
+++ upg1/plots/ex41.m
@@ -10,14 +10,10 @@ i=0;
 cd ../solidifying
 for T = Ts
   clear mcar;
-  i = i+1;
+  i = i+1;y
   file = sprintf('mcar-0%d_L%.2f_T%.2f_alpha%.2f_dt%s',N,L,T,alpha,dt);
   mcar = load(file);
-  subplot(5,2,i)
-  plot(mcar(:,1),mcar(:,2), 'o');
-  title(sprintf('T = %.1f',T));
-  xlabel('Step Number');
-  ylabel('Total Energy');
+  subplot(5,2,i)h
 end
 
 cd ../plots
